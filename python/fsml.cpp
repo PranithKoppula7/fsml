f82fcef2ab5c8ebf37b76a7a3bd0a7bb8a125378
Pranith Koppula <koppularaghavendra@gmail.com>
2024-01-16 22:39:24 -0700

    working on directory structure; not yet working

--- python/fsml.cpp
+++ python/fsml.cpp
@@ -1,12 +1,8 @@
 #include <pybind11/pybind11.h>
 
-namespace py = pybind11;
 
-float add(float arg1, float arg2) {
-  return arg1 + arg2;
-}
+namespace py = pybind11;
 
 PYBIND11_MODULE(fsml, handle) {
   handle.doc() = "fsml is a full stack tiny implementation of modern machine learning";
-  handle.def("float_add", &add);
 }
---
+++ test/ops.py
@@ -0,0 +1,2 @@
+import build.fsml as fsml
+
