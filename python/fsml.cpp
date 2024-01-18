9e03cfcb9185cd81d55ab01a8c221ad946977837
Pranith Koppula <koppularaghavendra@gmail.com>
2024-01-17 22:54:29 -0700

    testing stuf

--- python/fsml.cpp
+++ python/fsml.cpp
@@ -1,8 +1,10 @@
 #include <pybind11/pybind11.h>
+#include "fsml/opps.h"
 
 
 namespace py = pybind11;
 
 PYBIND11_MODULE(fsml, handle) {
   handle.doc() = "fsml is a full stack tiny implementation of modern machine learning";
+  handle.def("float_add", &add);
 }
