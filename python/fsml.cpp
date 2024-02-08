af26a4135040d543f4b3af94261a963ee4da156a
Pranith Koppula <koppularaghavendra@gmail.com>
2024-02-07 21:53:37 -0700

    binded tensor to pybind11; figuring out float*

--- python/fsml.cpp
+++ python/fsml.cpp
@@ -1,8 +1,14 @@
 #include <pybind11/pybind11.h>
+#include "../fsml/tensor.h"
 
 namespace py = pybind11;
 
 
 PYBIND11_MODULE(core, handle) {
   handle.doc() = "fsml is a full stack tiny implementation of modern machine learning";
+  py::class_<tensor::tensor>(
+    handle, "Tensor"
+  )
+  .def(py::init<int>())
+  .def(py::init<int, float>());
 }
---
+++ test.py
@@ -0,0 +1,5 @@
+from build.python.Debug.core import Tensor
+
+t = Tensor(5);
+
+t2 = Tensor(5, 3.0);
