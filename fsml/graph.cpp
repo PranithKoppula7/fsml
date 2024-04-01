afa7c8ee7ceb786f41cf89f7fede8930563ce0bd
Pranith Koppula <koppularaghavendra@gmail.com>
2024-03-31 23:59:41 -0600

    working version of multilayer graph but bad formatting for op nodes

--- fsml/graph.cpp
+++ fsml/graph.cpp
@@ -1,9 +1,11 @@
 
 #include "graph.h"
+#include "operation.h"
 
 #include <graphviz/gvc.h>
 #include <graphviz/cgraph.h>
 
+#include <string>
 #include <map>
 #include <vector>
 
@@ -27,8 +29,7 @@ void graph::run(tensor t) {
 }
 
 void dfs(tensor* t, 
-         std::map<tensor*,
-         Agnode_t*>& graph_map, 
+         std::map<tensor*, Agnode_t*>& graph_map, 
          GVC_t* gvc,
          Agraph_t* r) {
 
@@ -38,9 +39,16 @@ void dfs(tensor* t,
         graph_map[t] = curr;
     }
 
+    if(t->parents_.size() == 0) return;
+
+    std::string op_name = t->ctx_->op_ + t->repr();
+    // Agnode_t* op = agnode(r, t->ctx_->op_.data(), 1);
+    Agnode_t* op = agnode(r, op_name.data(), 1);
+    agedge(r, graph_map[t], op, 0, 1);
     for (tensor* p: t->parents_) {
         dfs(p, graph_map, gvc, r);
-        Agedge_t *e = agedge(r, graph_map[t], graph_map[p], 0, 1);
+        agedge(r, op, graph_map[p], 0, 1);
+        // agedge(r, graph_map[t], graph_map[p], 0, 1);
     }
 }
 
