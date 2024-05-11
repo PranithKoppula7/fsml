61f16e40bb01e35b07ce525af2c9266ea82734d8
Pranith Koppula <koppularaghavendra@gmail.com>
2024-05-10 23:57:21 -0600

    added another buffer test for 3d case

--- fsml/tests/buffer_test.cpp
+++ fsml/tests/buffer_test.cpp
@@ -11,3 +11,11 @@ TEST(BufferTest, Initialize) {
     EXPECT_EQ(b.getShape(), std::vector<int>({1, 2}));
     EXPECT_EQ(b.getStrides(), std::vector<std::size_t>({8, 4}));
 }
+
+TEST(BufferTest, Initialize3DShape) {
+    std::vector<int> shape = {1, 2, 3};
+    buffer<float> b(shape);
+
+    EXPECT_EQ(b.getShape(), std::vector<int>({1, 2, 3}));
+    EXPECT_EQ(b.getStrides(), std::vector<std::size_t>({24, 12, 4}));
+}
