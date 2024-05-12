05b37d81465d72d8518eb411c956e44edaadf1f1
Pranith Koppula <koppularaghavendra@gmail.com>
2024-05-11 23:20:29 -0600

    added more tests to buffer

--- fsml/tests/buffer_test.cpp
+++ fsml/tests/buffer_test.cpp
@@ -19,3 +19,20 @@ TEST(BufferTest, Initialize3DShape) {
     EXPECT_EQ(b.getShape(), std::vector<int>({1, 2, 3}));
     EXPECT_EQ(b.getStrides(), std::vector<std::size_t>({24, 12, 4}));
 }
+
+TEST(BufferTest, InitializeNDShape) {
+    std::vector<int> shape = {2, 3, 5, 10};
+    buffer<float> b(shape);
+
+    EXPECT_EQ(b.getShape(), std::vector<int>({2, 3, 5, 10}));
+    EXPECT_EQ(b.getStrides(), std::vector<std::size_t>({600, 200, 40, 4}));
+}
+
+TEST(BufferTest, InitializeShapeContains0_Throws) {
+    try {
+        std::vector<int> shape = {1, 2, 0};
+        buffer<float> b(shape);
+    } catch(std::invalid_argument e) {
+        EXPECT_STREQ(e.what(), "Shape cannot contain 0");
+    }
+}
